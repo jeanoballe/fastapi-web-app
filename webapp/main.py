1bb0c38de7b97388bbfe30d18e273a8fe3cb82cf
Jean Carlos Oballe Sarmiento <jeanoballe@gmail.com>
2022-12-12 17:44:38 -0300

    code refactor

--- webapp/main.py
+++ webapp/main.py
@@ -2,16 +2,38 @@ import fastapi
 import uvicorn
 import fastapi_chameleon
 from fastapi_chameleon import template
+from starlette.staticfiles import StaticFiles
+from webapp.views import account
+from webapp.views import home
+from webapp.views import packages
+
 
 app = fastapi.FastAPI()
-fastapi_chameleon.global_init('webapp/templates')
 
-@app.get("/")
-@template(template_file='index.html')
-def index(user: str = 'anon'):
-    # return {'user_name': user if user else 'anon'}
-    return {'user_name': user}
+
+def main():
+    configure()
+    uvicorn.run(app, host='127.0.0.1', port=8080)
+
+def configure():
+    configure_templates()
+    configure_routes()
+
+def configure_templates():
+    fastapi_chameleon.global_init('webapp/templates/')
+
+def configure_routes():
+    app.mount('/static', StaticFiles(directory='webapp/static'), name='static')
+    app.include_router(home.router)
+    app.include_router(account.router)
+    app.include_router(packages.router)
+
+def configure():
+    configure_templates()
+    configure_routes()
 
 
 if __name__ == '__main__':
-    uvicorn.run(app)
+    main()
+else:
+    configure()
